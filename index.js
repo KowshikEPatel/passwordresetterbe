cbcedfad4118ef1d9dbe90d768fab298b828a082
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-04 00:24:58 +0530

    generating email and sending reset link

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express  = require("express")
 const app = express()
 const mongodb = require('mongodb')
 const mongoclient = mongodb.MongoClient;
-const dbURL = process.env.DB_URL
+const dbURL = "mongodb+srv://m001-student:m001-student@cluster0.qslea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"//process.env.DBURL||'mongodb://mongodb0.example.com:27017'process.env.DB_URL
 const bcrypt = require('bcrypt')
 const cors = require('cors')
+const randomstring = require('randomstring')
+const nodemailer = require("nodemailer");
 const port = process.env.PORT||8000
 
 app.use(express.json())
@@ -39,7 +41,7 @@ app.post("/",cors(), async (req,res)=>{
 })
 
 //creating a new  user
-app.post("/newuser",async (req,res)=>{
+app.post("/newuser",cors(),async (req,res)=>{
     
     bcrypt.genSalt(11,(err,salt)=>{
         bcrypt.hash(req.body["password"],salt, async (err,hash)=>{
@@ -57,5 +59,43 @@ app.post("/newuser",async (req,res)=>{
         })
     })
     
+})
+
+app.post("/forgotpw",cors(),async (req,res)=>{
+    const client = await mongoclient.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
+    let db = client.db('projectrestapi')
+    let data = await db.collection("passreset").findOne(req.body)
+
+    let transporter = nodemailer.createTransport({ 
+        host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "felicia24@ethereal.email", // generated ethereal user
+      pass: "JYqAD1zNNwhS7Tc82d"      , // generated ethereal password
+    },
+  });
+
+  let randomURL = `https://kp-passwordresetter.herokuapp.com/resetpassword/${data["user_name"]}`+randomstring.generate()
+  let stored  = await db.collection('passreset').findOneAndUpdate(req.body,{$set:{"randomString":randomURL}})
+  let info = await transporter.sendMail({
+    from: '"felicia24@ethereal.email" <felicia24@ethereal.email>', // sender address
+    to:  data["email"], // list of receivers "bar@example.com, baz@example.com"
+    subject: "Password reset string", // Subject line
+    html: `<p>Hi ${data["user_name"]}, you have requested for resetting your password on onlogger.com. Click on the 
+    <a href="${randomURL}">link</a> to reset your password </p> `, // html body
+  });
+
+    res.status(200).json({info,stored})
+})
+
+app.get("/resetpassword/:user/:str",cors(),async(req,res)=>{
+
+    const client = await mongoclient.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
+    let db = client.db('projectrestapi')
+    let data = await db.collection("passreset").findOne({"user_name":req.params.user})
+    console.log(data["randomString"])
+    console.log(req.params.str)
+
 })
 app.listen(port,()=>console.log("app started at "+port))
